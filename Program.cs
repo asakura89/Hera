481c86f8789d27593b8b143df43d13688526a4ae
asakura89 <flamboyan3no1a@gmail.com>
2021-11-14 11:16:54 +0700

    [Initial] .

---
+++ Program.cs
@@ -0,0 +1,17 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+
+WebApplication app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.Run();
